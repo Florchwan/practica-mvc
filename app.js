97d944fe4fadc2cd81b5adf185e7ea6bf3f6b87b
Florchwan <florenciapetronacci@gmail.com>
2022-09-11 15:08:33 -0300

    micro desafio 6

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const express = require('express');
+const express = require("express");
+const path = require ("path");
+const mainRoutes = require ("./routers/main");
 
 const app = express();
+
+app.use (express.static ("public"));
+
+app.use (mainRoutes);
+
+app.get ('/', (req, res) => {
+res.sendFile(path.join (__dirname, '/views/home.html'))
+});
+
+app.get ('/about', (req, res) => {
+    res.sendFile(path.join (__dirname, '/views/about.html'))
+    });
+
+
 
 app.listen(3000, () => {
     console.log("Servidor funcionando en http://localhost:3000");
